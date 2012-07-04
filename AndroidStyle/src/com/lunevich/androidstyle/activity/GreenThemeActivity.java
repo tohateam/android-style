42c955925752493321b67dfc5cf4619aa2eef818
andrey.lunevich@gmail.com <andrey.lunevich@gmail.com@dba0a2d8-c655-5e24-2058-1c324f16987c>
2012-07-04 20:44:53 +0000

    Added listView style.

--- AndroidStyle/src/com/lunevich/androidstyle/activity/GreenThemeActivity.java
+++ AndroidStyle/src/com/lunevich/androidstyle/activity/GreenThemeActivity.java
@@ -1,25 +1,34 @@
 /**
  * 
  */
-package com.lunevich.androidstyle;
+package com.lunevich.androidstyle.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.lunevich.androidstyle.R;
 
 /**
  * @author andrey_lunevich
  * 
  */
-public class GreenStyleActivity extends Activity implements OnClickListener {
+public class GreenThemeActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTheme(R.style.Theme_Green);
 		setContentView(R.layout.stylish);
+		
+		String[] items = { "One", "Two", "Three", "Four", "Five" };
+		ListView listView = (ListView) findViewById(R.id.listView);
+		listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+		
 		Button btn = (Button) findViewById(R.id.btn);
 		btn.setOnClickListener(this);
 	}
